be9fa56644c2aec49a1ecdc058658c71a733bb44
martin.glatz <martin.glatz@192.168.0.248>
2023-01-22 13:21:39 +0100

    Restructured Testcases

--- amlValidationSuite/src/test/java/amlValidationTestSuite/AMLInstanceRequirements.java
+++ amlValidationSuite/src/test/java/amlValidationTestSuite/AMLInstanceRequirements.java
@@ -9,10 +9,18 @@ public class AMLInstanceRequirements extends AMLTest{
 
 	private String TestModelPath = "AMLInstanceRequirements\\";
 	
+	
+	
+	/************************************************************************************************************************/
+	
+	/*  TEST CASES FACETS
+	 *  
+	 */
+	
 	@Test
 	public void AMLFacetAttributInvalid() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLFacetAttributInvalid\\";		
+		String modelPath = TestModelPath + "Facet\\AMLFacetAttributInvalid\\";		
 		
 		addExpectedTestResult("Testcase.aml", "AreFacetAttributesValid", "", "", "Facet Attributes not defined: WRONG ATTRIBUTE ");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -21,7 +29,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void AMLFacetInterfaceInvalid() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLFacetInterfaceInvalid\\";		
+		String modelPath = TestModelPath + "Facet\\AMLFacetInterfaceInvalid\\";		
 		
 		addExpectedTestResult("Testcase.aml", "AreFacetInterfacesValid", "", "", "Facet Interfaces not defined: WRONG INTERFACE ");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -30,7 +38,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void AMLFacetInvalidIEChild() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLFacetInvalidIEChild\\";		
+		String modelPath = TestModelPath + "Facet\\AMLFacetInvalidIEChild\\";		
 		
 		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInFacets", "", "", "Invalid childs in Facet HMIFacet: InternalElement");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -41,7 +49,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void AMLFacetNameNotUnique() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLFacetNameNotUnique\\";		
+		String modelPath = TestModelPath + "Facet\\AMLFacetNameNotUnique\\";		
 		
 		addExpectedTestResult("Testcase.aml", "FacetNameIsUniqueWithinSiblings", "", "", "Duplicate Facet name detected");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -51,38 +59,12 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void AMLFacetValid() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLFacetValid\\";
+		String modelPath = TestModelPath + "Facet\\AMLFacetValid\\";
 		
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
-	@Test
-	public void PropertySetAttributeNotFoundInIE() throws Exception 
-	{
-		String modelPath = TestModelPath + "PropertySetAttributeNotFoundInIE\\";		
-		
-		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "x not found in IE");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
 	
-	@Test
-	public void PropertySetAttributeNotFoundInRoleReq() throws Exception 
-	{
-		String modelPath = TestModelPath + "PropertySetAttributeNotFoundInRoleReq\\";		
-		
-		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "Laenge not found in RoleRequirements");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
-	
-	@Test
-	public void PropertySetAttributeNotFoundInRC() throws Exception 
-	{
-		String modelPath = TestModelPath + "PropertySetAttributeNotFoundInRC\\";		
-		
-		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "Laenge not found in RC");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
 	
 	
 	@Test
@@ -100,12 +82,15 @@ public class AMLInstanceRequirements extends AMLTest{
 	
 /************************************************************************************************************************/
 	
+	/*  TEST CASES GROUP
+	 *  
+	 */
 	
 		
 	@Test
 	public void AMLGroupValid() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLGroupValid\\";		
+		String modelPath = TestModelPath + "Group\\AMLGroupValid\\";		
 		
 		//addExpectedTestResult("Testcase.aml", "IsDerivedFromAMLBaseClass", "", "", "Class not derived from AMLInterfaceclass: InterfaceClass");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -114,57 +99,23 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void AMLGroupWrongIENoMirror() throws Exception 
 	{
-		String modelPath = TestModelPath + "AMLGroupWrongIENoMirror\\";		
+		String modelPath = TestModelPath + "Group\\AMLGroupWrongIENoMirror\\";		
 		
 		addExpectedTestResult("Testcase.aml", "AreGroupChildrenValid", "", "", "Invalid child object detected");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
 	
-	/************************************************************************************************************************/	
-	
-	@Test
-	public void IE_ConnectedPortsWithDifferentCategories() throws Exception 
-	{
-		String modelPath = TestModelPath + "IE_ConnectedPortsWithDifferentCategories\\";		
-		
-		addExpectedTestResult("Testcase.aml", "AreConnectedPortOfSameCategory", "", "", "Non matching Categories: Category MaterialFlow doesn't match AnotherMaterialFlow");
-		
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
-	@Test
-	public void IE_ExternalDataConnector_EmptyRefUI() throws Exception 
-	{
-		String modelPath = TestModelPath + "IE_ExternalDataConnector_EmptyRefUI\\";		
-		
-		addExpectedTestResult("Testcase.aml", "HasExternalDataConnectorRefURI", "", "", "Instance of Type ExternalDataConnector has empty refURI: InterfaceClass");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
-	@Test
-	public void IE_ExternalDataConnector_WithoutRefUI() throws Exception 
-	{
-		String modelPath = TestModelPath + "IE_ExternalDataConnector_WithoutRefUI\\";		
-		
-		addExpectedTestResult("Testcase.aml", "HasExternalDataConnectorRefURI", "", "", "Instance of Type ExternalDataConnector doesn't have attribute refURI: InterfaceClass");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
-	
-	@Test
-	public void IE_InteralLinkPortCorrect() throws Exception 
-	{
-		String modelPath = TestModelPath + "IE_InteralLinkPortCorrect\\";		
-			
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
+/************************************************************************************************************************/	
+	/*  TEST CASES ORDER
+	 *  
+	 */	
 	
 	
 	@Test
 	public void IE_OrderWithoutDirection() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_OrderWithoutDirection\\";		
+		String modelPath = TestModelPath + "Order\\IE_OrderWithoutDirection\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirection", "", "", "Instance of Type Order doesn't have attribute Direction: InterfaceClass");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -173,16 +124,60 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void IE_OrderWithWrongDirectionAttribute() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_OrderWithWrongDirectionAttribute\\";		
+		String modelPath = TestModelPath + "Order\\IE_OrderWithWrongDirectionAttribute\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirectionCorrectValue", "", "", "Instance of Type Order has invalid value Instance: InterfaceClass");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
 	@Test
+	public void SUC_OrderWithoutDirection() throws Exception 
+	{
+		String modelPath = TestModelPath + "Order\\SUC_OrderWithoutDirection\\";		
+		
+		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirection", "", "", "Instance of Type Order doesn't have attribute Direction: InterfaceClass");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	@Test
+	public void SUC_OrderWithWrongDirectionAttribute() throws Exception 
+	{
+		String modelPath = TestModelPath + "Order\\SUC_OrderWithWrongDirectionAttribute\\";		
+		
+		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirectionCorrectValue", "", "", "Instance of Type Order has invalid value Instance: InterfaceClass");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	
+	/************************************************************************************************************************/	
+	/*  TEST CASES PORT
+	 *  
+	 */	
+	
+	
+	@Test
+	public void IE_ConnectedPortsWithDifferentCategories() throws Exception 
+	{
+		String modelPath = TestModelPath + "Port\\IE_ConnectedPortsWithDifferentCategories\\";		
+		
+		addExpectedTestResult("Testcase.aml", "AreConnectedPortOfSameCategory", "", "", "Non matching Categories: Category MaterialFlow doesn't match AnotherMaterialFlow");
+		
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}	
+	
+	
+	@Test
+	public void IE_InteralLinkPortCorrect() throws Exception 
+	{
+		String modelPath = TestModelPath + "Port\\IE_InteralLinkPortCorrect\\";		
+			
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}	
+	
+	@Test
 	public void SUC_PortWithInteralElementChilds() throws Exception 
 	{
-		String modelPath = TestModelPath + "SUC_PortWithInteralElementChilds\\";		
+		String modelPath = TestModelPath + "Port\\SUC_PortWithInteralElementChilds\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortNoChildInternalElements", "", "", "Instance of Type Port has child elements InternalElement: SystemUnitClass1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -191,7 +186,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void IE_PortWithoutExternalInterface() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_PortWithoutExternalInterface\\";		
+		String modelPath = TestModelPath + "Port\\IE_PortWithoutExternalInterface\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortExternalInterface", "", "", "Port without ExternalInterface (exkl PortConnector): InternalElement10000");
 		
@@ -201,7 +196,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void SUC_PortWithoutPortConnector() throws Exception 
 	{
-		String modelPath = TestModelPath + "SUC_PortWithoutPortConnector\\";		
+		String modelPath = TestModelPath + "Port\\SUC_PortWithoutPortConnector\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortConnector", "", "", "Instance of Type Port has no ExternalInterface PortConnector: SystemUnitClass1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -210,7 +205,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void IE_PortWithWrongCardinalityAttribute() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_PortWithWrongCardinalityAttribute\\";		
+		String modelPath = TestModelPath + "Port\\IE_PortWithWrongCardinalityAttribute\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortCardinalityCorrectValue", "", "", "Instance of Type Port has invalid value Cardinality Instance: InternalElement1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -219,35 +214,18 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void IE_PortWithWrongDirectionAttribute() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_PortWithWrongDirectionAttribute\\";		
+		String modelPath = TestModelPath + "Port\\IE_PortWithWrongDirectionAttribute\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortDirectionCorrectValue", "", "", "Instance of Type Port has invalid value Instance: InternalElement1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
-	@Test
-	public void SUC_OrderWithoutDirection() throws Exception 
-	{
-		String modelPath = TestModelPath + "SUC_OrderWithoutDirection\\";		
-		
-		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirection", "", "", "Instance of Type Order doesn't have attribute Direction: InterfaceClass");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
-	@Test
-	public void SUC_OrderWithWrongDirectionAttribute() throws Exception 
-	{
-		String modelPath = TestModelPath + "SUC_OrderWithWrongDirectionAttribute\\";		
-		
-		addExpectedTestResult("Testcase.aml", "HasOrderInstanceDirectionCorrectValue", "", "", "Instance of Type Order has invalid value Instance: InterfaceClass");
-		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
 	
 
 	@Test
 	public void IE_PortWithInteralElementChilds() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_PortWithInteralElementChilds\\";		
+		String modelPath = TestModelPath + "Port\\IE_PortWithInteralElementChilds\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortNoChildInternalElements", "", "", "Instance of Type Port has child elements InternalElement: InternalElement1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -256,18 +234,17 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void IE_PortWithoutPortConnector() throws Exception 
 	{
-		String modelPath = TestModelPath + "IE_PortWithoutPortConnector\\";		
+		String modelPath = TestModelPath + "Port\\IE_PortWithoutPortConnector\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortConnector", "", "", "Instance of Type Port has no ExternalInterface PortConnector: InternalElement1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
-	}
-	
+	}	
 	
 
 	@Test
 	public void SUC_PortWithWrongCardinalityAttribute() throws Exception 
 	{
-		String modelPath = TestModelPath + "SUC_PortWithWrongCardinalityAttribute\\";		
+		String modelPath = TestModelPath + "Port\\SUC_PortWithWrongCardinalityAttribute\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortCardinalityCorrectValue", "", "", "Instance of Type Port has invalid value Cardinality Instance: SystemUnitClass");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -277,7 +254,7 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void SUC_PortWithWrongDirectionAttribute() throws Exception 
 	{
-		String modelPath = TestModelPath + "SUC_PortWithWrongDirectionAttribute\\";		
+		String modelPath = TestModelPath + "Port\\SUC_PortWithWrongDirectionAttribute\\";		
 		
 		addExpectedTestResult("Testcase.aml", "HasPortDirectionCorrectValue", "", "", "Instance of Type Port has invalid value Instance: SystemUnitClass1");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
@@ -288,9 +265,9 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void PropertySetInvalidIEChild() throws Exception 
 	{
-		String modelPath = TestModelPath + "PropertySetInvalidIEChild\\";		
+		String modelPath = TestModelPath + "PropertySet\\PropertySetInvalidIEChild\\";		
 		
-		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInFacets", "", "", "Invalid childs in PropertySet Maﬂe: InternalElement");
+		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInPropertySet", "", "", "Invalid childs in PropertySet Maﬂe: InternalElement");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
@@ -298,9 +275,9 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void PropertySetInvalidAttribut() throws Exception 
 	{
-		String modelPath = TestModelPath + "PropertySetInvalidAttribut\\";		
+		String modelPath = TestModelPath + "PropertySet\\PropertySetInvalidAttribut\\";		
 		
-		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInFacets", "", "", "Invalid childs in PropertySet Maﬂe: Attribut");
+		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInPropertySet", "", "", "Invalid childs in PropertySet Maﬂe: Attribut");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
@@ -309,17 +286,67 @@ public class AMLInstanceRequirements extends AMLTest{
 	@Test
 	public void PropertySetInvalidInterface() throws Exception 
 	{
-		String modelPath = TestModelPath + "PropertySetInvalidInterface\\";		
+		String modelPath = TestModelPath + "PropertySet\\PropertySetInvalidInterface\\";		
 		
-		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInFacets", "", "", "Invalid childs in PropertySet Maﬂe: Interface");
+		addExpectedTestResult("Testcase.aml", "NoAdditionalChildElementsInPropertySet", "", "", "Invalid childs in PropertySet Maﬂe: Interface");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
 	@Test
+	public void PropertySetAttributeNotFoundInIE() throws Exception 
+	{
+		String modelPath = TestModelPath + "PropertySet\\PropertySetAttributeNotFoundInIE\\";		
+		
+		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "x not found in IE");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	@Test
+	public void PropertySetAttributeNotFoundInRoleReq() throws Exception 
+	{
+		String modelPath = TestModelPath + "PropertySet\\PropertySetAttributeNotFoundInRoleReq\\";		
+		
+		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "Laenge not found in RoleRequirements");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	
+	@Test
+	public void PropertySetAttributeNotFoundInRC() throws Exception 
+	{
+		String modelPath = TestModelPath + "PropertySet\\PropertySetAttributeNotFoundInRC\\";		
+		
+		addExpectedTestResult("Testcase.aml", "ArePropertySetMappingsValid", "", "", "Laenge not found in RC");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	
+	
+	@Test
 	public void PropertySetValid() throws Exception 
 	{
-		String modelPath = TestModelPath + "PropertySetIValid\\";		
+		String modelPath = TestModelPath + "PropertySet\\PropertySetIValid\\";		
 		
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	/***************************************************************************************************************************************************/	
+	
+	@Test
+	public void IE_ExternalDataConnector_EmptyRefUI() throws Exception 
+	{
+		String modelPath = TestModelPath + "ExternalDataCon\\IE_ExternalDataConnector_EmptyRefUI\\";		
+		
+		addExpectedTestResult("Testcase.aml", "HasExternalDataConnectorRefURI", "", "", "Instance of Type ExternalDataConnector has empty refURI: InterfaceClass");
+		Assertions.assertTrue(executeAndValidateTest(modelPath));		
+	}
+	
+	@Test
+	public void IE_ExternalDataConnector_WithoutRefUI() throws Exception 
+	{
+		String modelPath = TestModelPath + "ExternalDataCon\\IE_ExternalDataConnector_WithoutRefUI\\";		
+		
+		addExpectedTestResult("Testcase.aml", "HasExternalDataConnectorRefURI", "", "", "Instance of Type ExternalDataConnector doesn't have attribute refURI: InterfaceClass");
 		Assertions.assertTrue(executeAndValidateTest(modelPath));		
 	}
 	
